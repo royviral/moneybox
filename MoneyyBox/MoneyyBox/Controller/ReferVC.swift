03f5c8db92a9139070e380bc438f9af358ff250d
viral <viral@ABs.local>
2019-02-03 14:04:37 +0530

    03-02-2019 -send invitation by sms implemented

--- MoneyyBox/MoneyyBox/Controller/ReferVC.swift
+++ MoneyyBox/MoneyyBox/Controller/ReferVC.swift
@@ -8,9 +8,34 @@
 
 import UIKit
 import Alamofire
+import Toast_Swift
 import SwiftyJSON
+import MessageUI
 
-class ReferVC: BaseViewController {
+class ReferVC: BaseViewController, MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+        switch result.rawValue {
+        case MessageComposeResult.sent.rawValue:
+            
+            let alertVC = CPAlertVC.create().config(title: "Success!", message: "Reference link sent successfully.")
+            
+            alertVC.addAction(CPAlertAction(title: "Ok", type: .normal, handler: {
+                
+            }))
+            alertVC.show(into: self)
+        case MessageComposeResult.failed.rawValue:
+            let alertVC = CPAlertVC.create().config(title: "Error!", message: "Not able to send text message. Try again.")
+            
+            alertVC.addAction(CPAlertAction(title: "Ok", type: .normal, handler: {
+                
+            }))
+            alertVC.show(into: self)
+        default:
+            break
+        }
+    }
+    
 
     @IBOutlet weak var userEmail: UITextField!
     override func viewDidLoad() {
@@ -22,22 +47,36 @@ class ReferVC: BaseViewController {
     @IBAction func btnMenuClick(_ sender: UIButton) {
         onSlideMenuButtonPressed(sender)
     }
+
     
     @IBAction func inviteBtn(_ sender: Any) {
         if(userEmail.text == ""){
-            let alertVC = CPAlertVC.create().config(title: "Error!", message: "Please enter an email.")
+            let alertVC = CPAlertVC.create().config(title: "Error!", message: "Please enter valid phone number.")
             
             alertVC.addAction(CPAlertAction(title: "Ok", type: .normal, handler: {
                 
             }))
             alertVC.show(into: self)
         }else{
-            let alertVC = CPAlertVC.create().config(title: "Success!", message: "Reference link sent successfully.")
             
-            alertVC.addAction(CPAlertAction(title: "Ok", type: .normal, handler: {
+            let composeVC = MFMessageComposeViewController()
+            composeVC.messageComposeDelegate = self
+            composeVC.recipients = [userEmail.text ] as? [String]
+            composeVC.body = "Hi, This is kazoom application link. You can direct download form http://www.kazoom.com"
+            
+            if MFMessageComposeViewController.canSendText(){
+                userEmail.text = ""
+                self.present(composeVC, animated: true, completion: nil)
                 
-            }))
-            alertVC.show(into: self)
+                
+            }else{
+                let alertVC = CPAlertVC.create().config(title: "Error!", message: "Not able to send text message. Try again.")
+                
+                alertVC.addAction(CPAlertAction(title: "Ok", type: .normal, handler: {
+                    
+                }))
+                alertVC.show(into: self)
+            }
         }
     }
 }
